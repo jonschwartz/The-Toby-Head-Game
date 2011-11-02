13c17ad4e4632fe1244a3c43938f7ca26665f4d6
Jon Schwartz <jschwartz@hmcelectronics.com>
2011-11-02 08:57:30 -0400

    added in prizes

--- gen/com/jonschwartzsoftware/fencethehead/R.java
+++ gen/com/jonschwartzsoftware/fencethehead/R.java
@@ -13,20 +13,28 @@ public final class R {
     public static final class drawable {
         public static final int fth_splash=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int toby_head_facing_left=0x7f020002;
-        public static final int toby_head_facing_right=0x7f020003;
+        public static final int poo=0x7f020002;
+        public static final int toby_head_facing_left=0x7f020003;
+        public static final int toby_head_facing_right=0x7f020004;
+        public static final int turtle=0x7f020005;
     }
     public static final class id {
-        public static final int adView=0x7f060005;
-        public static final int bPlayAgain=0x7f060004;
-        public static final int tvHighScore=0x7f060003;
-        public static final int tvHighScoreLabel=0x7f060002;
-        public static final int tvScore=0x7f060001;
-        public static final int tvScoreLabel=0x7f060000;
+        public static final int adView=0x7f070005;
+        public static final int bPlayAgain=0x7f070004;
+        public static final int ivPrize=0x7f070007;
+        public static final int miResetScore=0x7f070008;
+        public static final int tvHighScore=0x7f070003;
+        public static final int tvHighScoreLabel=0x7f070002;
+        public static final int tvScore=0x7f070001;
+        public static final int tvScoreLabel=0x7f070000;
+        public static final int tvYouWon=0x7f070006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int score=0x7f030001;
+    }
+    public static final class menu {
+        public static final int scoremenu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
--- src/com/jonschwartzsoftware/fencethehead/Score.java
+++ src/com/jonschwartzsoftware/fencethehead/Score.java
@@ -5,34 +5,42 @@ import com.google.ads.*;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class Score extends Activity implements OnClickListener{
+public class Score extends Activity implements OnClickListener {
 
 	Button playAgain;
-	TextView tvscore, scorelabel, tvhighscore;
+	TextView tvscore, scorelabel, tvhighscore, youWin;
+	ImageView prize;
 	int finalScore;
 	Bundle gotScore;
 	AdView adview;
 	LinearLayout llAd;
-	
-	
+	SharedPreferences settings;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.score);
 		setup();
-	    
+
 	}
 
 	private void setup() {
@@ -42,23 +50,50 @@ public class Score extends Activity implements OnClickListener{
 		scorelabel = (TextView) findViewById(R.id.tvScoreLabel);
 		tvhighscore = (TextView) findViewById(R.id.tvHighScore);
 		adview = (AdView) findViewById(R.id.adView);
-		
-		SharedPreferences settings = getSharedPreferences("highscore", 0);
-	    int highscore = settings.getInt("highscore", 0);
-		
+		prize = (ImageView) findViewById(R.id.ivPrize);
+		youWin = (TextView) findViewById(R.id.tvYouWon);
+
+		settings = getSharedPreferences("highscore", 0);
+		int highscore = settings.getInt("highscore", 0);
+
 		playAgain.setOnClickListener(this);
 		finalScore = 0;
 		gotScore = getIntent().getExtras();
 		finalScore = gotScore.getInt("score");
 		tvscore.setText(Integer.toString(finalScore));
-		if (finalScore > highscore){
+		if (finalScore > highscore) {
 			highscore = finalScore;
-		    SharedPreferences.Editor editor = settings.edit();
-		    editor.putInt("highscore", highscore);
-		    editor.commit();
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putInt("highscore", highscore);
+			editor.commit();
 		}
+
+		Resources res = getResources();
+
+		switch (finalScore) {
+		case 43:
+			youWin.setText("You won a Golden Poo!");
+			Drawable drawablePoo = res.getDrawable(R.drawable.poo);
+			prize.setImageDrawable(drawablePoo);
+			break;
+		case 81:
+			youWin.setText("You won a Bejemmed Turtle!");
+			Drawable drawableTurtle = res.getDrawable(R.drawable.turtle);
+			prize.setImageDrawable(drawableTurtle);
+			break;
+		}
+
 		tvhighscore.setText(Integer.toString(highscore));
 		adview.loadAd(new AdRequest());
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu);
+		MenuInflater blowUp = getMenuInflater();
+		blowUp.inflate(R.menu.scoremenu, menu);
+		return true;
 	}
 
 	public void onClick(View arg0) {
@@ -72,7 +107,23 @@ public class Score extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		super.onDestroy();
 		adview.destroy();
-	    super.onDestroy();
+		super.onDestroy();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		super.onOptionsItemSelected(item);
+
+		switch (item.getItemId()) {
+		case R.id.miResetScore:
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putInt("highscore", 0);
+			editor.commit();
+			tvhighscore.setText("0");
+			break;
+		}
+		return false;
 	}
 
 }
